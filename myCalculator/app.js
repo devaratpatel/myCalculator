c07d5a82ceb17d9b95f96030f6323ecf49ea14e1
Devarat Patel <devaratpatel1@gmail.com>
2021-09-20 00:44:09 -0400

    edit: background + title

--- myCalculator/app.js
+++ myCalculator/app.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-unused-vars */
 function calculate(inputObj) {
-  let num1 = parseFloat(document.calcForm.num1.value);
-  let num2 = parseFloat(document.calcForm.num2.value);
+  let number1 = parseFloat(document.calcForm.num1.value);
+  let number2 = parseFloat(document.calcForm.num2.value);
   let result = document.calcForm.result;
 
   if (inputObj.value === "Add") {
-    result.value = num1 + num2;
+    result.value = number1 + number2;
   } else if (inputObj.value === "Subtract") {
-    result.value = num1 - num2;
+    result.value = number1 - number2;
   } else if (inputObj.value === "Multiply") {
-    result.value = num1 * num2;
+    result.value = number1 * number2;
   } else if (inputObj.value === "Divide") {
-    result.value = num1 / num2;
+    result.value = number1 / number2;
   } else {
-    result.value = num1 % num2;
+    result.value = number1 % number2;
   }
 }
